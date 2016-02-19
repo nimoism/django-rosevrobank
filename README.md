780816a31bc6f5cc98c877fc14674cd8f902ed9d
sm <dmitry.puhov@gmail.com>
2016-02-20 01:05:07 +0300

    Initial project commit

---
+++ README.md
@@ -0,0 +1,1 @@
+RosEvroBank (http://rosevrobank.ru) integration for Django
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+import rosevrobank as meta
+
+
+def long_description():
+    with open('README.md') as f:
+        rst = f.read()
+        return rst
+
+setup(
+    name='django-rosevrobank',
+    version=meta.__version__,
+    description=meta.__doc__,
+    author=meta.__author__,
+    author_email=meta.__contact__,
+    long_description=long_description(),
+    url='https://github.com/nimoism/django-rosevrobank',
+    platforms=["any"],
+    packages=find_packages(),
+    install_requires=[
+        'python-rosevrobank-api',
+    ],
+    scripts=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Framework :: Django',
+    ]
+)
